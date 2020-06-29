e509c146fe25ef649c10c7161a09a3953d328f1c
Scott Young <50921071+thebotmakes@users.noreply.github.com>
2020-06-29 09:12:06 +0100

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -5,12 +5,12 @@ import scrollphat
 
 
 scrollphat.set_brightness(2)
-scrollphat.set_rotate(True)
+scrollphat.set_rotate(True) #I set this as true as on the zero usb cable meant to set it level message was upside down 
 
 while True:
     now = datetime.datetime.now()
     print(now.strftime("%Y-%m-%d %H:%M:&S"))
-    birthday = datetime.datetime(2020,7,25,0,0)
+    birthday = datetime.datetime(YYYY,M,D,0,0) #replace this with the day you want to use - time not important for this application
     print(birthday.strftime("%Y-%m-%d %H:%M:&S"))
     delta = birthday - now 
     print(delta.days)
@@ -20,7 +20,7 @@ while True:
         scroll_delta = 1
     else:
         message = str(delta.days + 1)
-        scroll_delta = 0
+        scroll_delta = 0 #because days is a short message don't need to scroll it
     print(message)
     scrollphat.write_string(message)
     length = scrollphat.buffer_len()
